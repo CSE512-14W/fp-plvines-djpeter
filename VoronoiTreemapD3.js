04a9a9b94a536bb0d2b8d0c8c0e3b15d21ad7db0
djpeterhenry <phenry@gmail.com>
2014-03-20 12:03:20 -0700

    continuing slow stupid work on D3

--- VoronoiTreemapD3.js
+++ VoronoiTreemapD3.js
@@ -27,20 +27,51 @@ function d3_layout_hierarchyLinks(nodes) {
 // the actual implementation
 
 d3.layout.voronoitreemap = function() {
-	var hierarchy = d3.layout.hierarchy(),
+	var hierarchy = d3.layout.hierarchy().sort(null),
 		//round = Math.round,
-		size = [1, 1], // width, height
+		//size = [1, 1], // width, height  // DON'T DO THIS,
+		root_polygon = [[0,0],[500,0],[500,500],[0,500]], // obviously stupid...set somehow
+		iterations = 100,
 		somenewvariable = 0;
 	
 	function voronoitreemap(d) {
 		var nodes = hierarchy(d),
 			root = nodes[0];
+
+		root.polygon = root_polygon;
+		root.site = null; // hmm?
+		computeDiagramRecursively(root);
+		
 		return nodes;
 	}
 
-	voronoitreemap.size = function(x) {
-		if (!arguments.length) return size;
-		size = x;
+	function computeDiagramRecursively(node) {
+		var children = node.children;
+		if (children && children.length) {
+			node.sites = VoronoiTreemap.init(node.polygon, dataset);  // can't say dataset
+			VoronoiTreemap.normalizeSites(node.sites);
+			VoronoiTreemap.sites = sites;
+			VoronoiTreemap.setClipPolygon(node.polygon);
+			VoronoiTreemap.useNegativeWeights = true;
+			VoronoiTreemap.cancelOnAreaErrorThreshold =  true;
+			var polygons = VoronoiTreemap.doIterate(iterations);
+			
+			// set children polygons and sites
+			for (var i = 0; i < children.length; i++) {
+				children[i].polygon = polygons[i];
+				children[i].site = VoronoiTreemap.sites[i];
+				// goes all the way down
+				//computeDiagramRecursively(children[i]);
+			}
+			
+			//children.forEach(computeDiagramRecursively);
+		}
+	}
+	
+	
+	voronoitreemap.root_polygon = function(x) {
+		if (!arguments.length) return root_polygon;
+		root_polygon = x;
 		return voronoitreemap;
 	};
 	 
