6f5bd741f2dd3745b4b820c4b44dc67a9e2a5bff
djpeterhenry <phenry@gmail.com>
2014-03-21 02:49:59 -0700

    paper_simple_example goes with simple-whatever
    DON'T use the centroids ones...they are there to show how there's an
    errant centroid for non-leaf nodes [node.site.x, node.site.y]

---
+++ paper_simple_example.js
@@ -0,0 +1,38 @@
+paper_simple_example = {
+"name": "root",
+"children": [
+	{
+	"name": "shallow",
+	"size": 5
+	},
+	{
+	"name": "medium",
+	"children": [
+		{"name": "medium1", "size": 1},
+		{"name": "medium2", "size": 2},
+		{"name": "medium3", "size": 3},
+		{"name": "medium4", "size": 4}
+	]
+	},
+	{
+	"name": "deep",
+	"children": [
+		{"name": "deep1", 
+		"children": [
+			{"name": "deep1leaf1", "size": 1},
+			{"name": "deep1leaf2", "size": 1},
+			{"name": "deep1leaf3", "size": 1},
+			{"name": "deep1leaf4", "size": 1},
+			{"name": "deep1leaf5", "size": 1}
+		]
+		},
+		{"name": "deep2", 
+		"children": [
+			{"name": "deep2leaf1", "size": 2},
+			{"name": "deep2leaf2", "size": 3}
+		]
+		}
+	]
+	}
+]
+}
